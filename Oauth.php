c147bae2a634fa7433e4ea68e2851abd1fcff189
Julien J <julien.joye@gmail.com>
2013-06-20 23:54:37 +0400

    Translate into a PHP Class

--- Oauth.php
+++ Oauth.php
@@ -1,132 +1,219 @@
 <?php
+
+// Defines twitter API constants
+define(TW_PROTOCOL, 'https://');
+define(TW_HOST, 'api.twitter.com');
+define(TW_VERSION, '1.1');
+define(TW_BASEURL, TW_PROTOCOL . TW_HOST .'/'. TW_VERSION);
+
 /**
-* 	Oauth.php
-*
-*	Created by Jon Hurlock on 2013-03-20.
-* 	
-*	Jon Hurlock's Twitter Application-only Authentication App by Jon Hurlock (@jonhurlock)
-*	is licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License.
-*	Permissions beyond the scope of this license may be available at http://www.jonhurlock.com/.
-*/
+ * OauthTwitter Class
+ * Used for authenticate your application and run request to Twitter API
+ *
+ *  Usage :
+ *    $obj = new OauthTwitter($consumer_key, $consumer_secret, $app_name);
+ *    $obj->getAccountStatuses($account);
+ *    $obj->getAccountInfos($account);
+ *    $obj->getSearchResults($term);
+ */
+class OauthTwitter {
 
+  private static $_instance;
 
-// fill in your consumer key and consumer secret below
-define('CONSUMER_KEY', 'enter_your_consumer_key_here');
-define('CONSUMER_SECRET', 'enter_your_consumer_secret_here');
-/**
-*	Get the Bearer Token, this is an implementation of steps 1&2
-*	from https://dev.twitter.com/docs/auth/application-only-auth
-*/
-function get_bearer_token(){
-	// Step 1
-	// step 1.1 - url encode the consumer_key and consumer_secret in accordance with RFC 1738
-	$encoded_consumer_key = urlencode(CONSUMER_KEY);
-	$encoded_consumer_secret = urlencode(CONSUMER_SECRET);
-	// step 1.2 - concatinate encoded consumer, a colon character and the encoded consumer secret
-	$bearer_token = $encoded_consumer_key.':'.$encoded_consumer_secret;
-	// step 1.3 - base64-encode bearer token
-	$base64_encoded_bearer_token = base64_encode($bearer_token);
-	// step 2
-	$url = "https://api.twitter.com/oauth2/token"; // url to send data to for authentication
-	$headers = array( 
-		"POST /oauth2/token HTTP/1.1", 
-		"Host: api.twitter.com", 
-		"User-Agent: jonhurlock Twitter Application-only OAuth App v.1",
-		"Authorization: Basic ".$base64_encoded_bearer_token."",
-		"Content-Type: application/x-www-form-urlencoded;charset=UTF-8", 
-		"Content-Length: 29"
-	); 
+  private $_app_name;
+  private $_consumer_key;
+  private $_consumer_secret;
 
-	$ch = curl_init();  // setup a curl
-	curl_setopt($ch, CURLOPT_URL,$url);  // set url to send to
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); // set custom headers
-	curl_setopt($ch, CURLOPT_POST, 1); // send as post
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // return output
-	curl_setopt($ch, CURLOPT_POSTFIELDS, "grant_type=client_credentials"); // post body/fields to be sent
-	$header = curl_setopt($ch, CURLOPT_HEADER, 1); // send custom headers
-	$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-	$retrievedhtml = curl_exec ($ch); // execute the curl
-	curl_close($ch); // close the curl
-	$output = explode("\n", $retrievedhtml);
-	$bearer_token = '';
-	foreach($output as $line)
-	{
-		if($line === false)
-		{
-			// there was no bearer token
-		}else{
-			$bearer_token = $line;
-		}
-	}
-	$bearer_token = json_decode($bearer_token);
-	return $bearer_token->{'access_token'};
+  private $_consumer_token;
+  private $_bearer_token;
+
+  /**
+   * On object construction, populate private variables with consumer params
+   */
+  function __construct($consumer_key, $consumer_secret, $app_name = 'Twitter Application-only OAuth App v.1')
+  {
+    // Defines client params
+    $this->_consumer_key = $consumer_key;
+    $this->_consumer_secret  = $consumer_secret;
+    $this->_app_name = $app_name;
+
+    // Encode the consumer token for authentication
+    $this->_setConsumerToken();
+
+    // Define Bearer token for requests
+    $this->_setBearerToken();
+  }
+
+  /**
+   * On object destruction, we invalidate the Bearer Token
+   */
+  function __destruct()
+  {
+    $this->_invalidateBearerToken();
+  }
+
+  /**
+   * Set the Consumer Token.
+   * Used to get, or invalidates the Bearer Token
+   */
+  private function _setConsumerToken()
+  {
+    // Url encode the consumer_key and consumer_secret in accordance with RFC 1738
+    $encoded_consumer_key = urlencode($this->_consumer_key);
+    $encoded_consumer_secret = urlencode($this->_consumer_secret);
+
+    $this->_consumer_token = base64_encode($encoded_consumer_key .':'. $encoded_consumer_secret);
+  }
+
+  /**
+   * Get and set the Bearer Token.
+   * Authenticates the Application with oauth2
+   */
+  private function _setBearerToken()
+  {
+    $endpoint = '/oauth2/token';
+    $url = TW_PROTOCOL . TW_HOST . $endpoint;
+    $headers = array(
+      'POST '. $end_point .' HTTP/1.1',
+      'Host: '. TW_HOST,
+      'User-Agent: '. $this->_app_name,
+      'Authorization: Basic '. $this->_consumer_token,
+      'Content-Type: application/x-www-form-urlencoded;charset=UTF-8',
+      'Content-Length: 29',
+    );
+
+    try
+    {
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, array('grant_type' => 'client_credentials'));
+      $header = curl_setopt($ch, CURLOPT_HEADER, 1);
+      $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+      $res = curl_exec($ch);
+      curl_close($ch);
+      $bearer_token = json_decode(end(explode("\n", $res)));
+
+      $this->_bearer_token = $bearer_token->access_token;
+    }
+    catch (HttpException $ex) {
+      throw new Exception('Could not retrieve data from Twitter. '. $ex);
+    }
+  }
+
+  /**
+   * Invalidates the Bearer Token
+   * Should the bearer token become compromised or need to be invalidated for any reason,
+   * Call this method.
+   */
+  private function _invalidateBearerToken()
+  {
+    $endpoint = '/oauth2/invalidate_token';
+    $url = TW_PROTOCOL . TW_HOST . $endpoint;
+    $headers = array(
+      'POST '. $endpoint .' HTTP/1.1',
+      'Host: '. TW_HOST,
+      'User-Agent: '. $this->_app_name,
+      'Authorization: Basic '. $this->_consumer_token,
+      'Accept: */*',
+      'Content-Type: application/x-www-form-urlencoded',
+      'Content-Length: '. strlen($this->_consumer_token) + 13
+    );
+
+    try {
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, array('access_token' => $this->_bearer_token));
+      $header = curl_setopt($ch, CURLOPT_HEADER, 1);
+      $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+      $return = curl_exec($ch);
+      curl_close($ch);
+    }
+    catch (HttpException $ex) {
+      throw new Exception('Could not retrieve data from Twitter. '. $ex);
+    }
+
+    return $return;
+  }
+
+  /**
+   * Send Basic GET Requests to the Twitter API
+   */
+  private function _getHttpRequest($endpoint, $data)
+  {
+    $headers = array(
+      'GET: /'. TW_VERSION . $endpoint .' HTTP/1.1',
+      'Host: '. TW_HOST,
+      'User-Agent:'. $this->_app_name,
+      'Authorization: Bearer '. $this->_bearer_token,
+    );
+
+    $ch = curl_init(TW_BASEURL . $endpoint . $data);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_POST, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $header = curl_setopt($ch, CURLOPT_HEADER, 1);
+    $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    try {
+      $return = curl_exec($ch);
+      curl_close($ch);
+      $data = end(explode("\n", $return));
+    }
+    catch (HttpException $ex) {
+      throw new Exception('Could not retrieve data from Twitter. '. $ex);
+    }
+
+    return json_decode($data);
+  }
+
+  /**
+   * Search
+   * Basic Tweets Search of the Search API
+   * Based on https://dev.twitter.com/docs/api/1.1/get/search/tweets
+   */
+  public function getSearchResults($query, $result_type = 'mixed', $count = 5, $entities = TRUE)
+  {
+    $entities = $entities ? 'true' : 'false';
+    $endpoint = '/search/tweets.json';
+    $data = '?q='. $query .'&result_type='. $result_type .'&count='. $count .'&include_entities='. $entities;
+
+    return $this->_getHttpRequest($endpoint, $data);
+  }
+
+  /**
+   * User Timeline
+   * Basic User Timeline of the Statuses API
+   * Based on https://dev.twitter.com/docs/api/1.1/get/statuses/user_timeline
+   */
+  public function getAccountStatuses($screen_name, $result_type = 'recent', $count = 2)
+  {
+    $endpoint = '/statuses/user_timeline.json';
+    $data = '?screen_name='. ltrim($screen_name, '@') .'&count='. $count .'&result_type='. $result_type .'&rpp='. $count .'&include_entities=true';
+
+    return $this->_getHttpRequest($endpoint, $data);
+  }
+
+  /**
+   * User Informations
+   * Basic User of the Users API
+   * Based on https://dev.twitter.com/docs/api/1.1/get/users/show
+   */
+  public function getAccountInfos($screen_name)
+  {
+    $endpoint = '/users/show.json'; // base url
+    $data = '?screen_name='. ltrim($screen_name, '@');
+
+    return $this->_getHttpRequest($endpoint, $data);
+  }
+
 }
-
-/**
-* Invalidates the Bearer Token
-* Should the bearer token become compromised or need to be invalidated for any reason,
-* call this method/function.
-*/
-function invalidate_bearer_token($bearer_token){
-	$encoded_consumer_key = urlencode(CONSUMER_KEY);
-	$encoded_consumer_secret = urlencode(CONSUMER_SECRET);
-	$consumer_token = $encoded_consumer_key.':'.$encoded_consumer_secret;
-	$base64_encoded_consumer_token = base64_encode($consumer_token);
-	// step 2
-	$url = "https://api.twitter.com/oauth2/invalidate_token"; // url to send data to for authentication
-	$headers = array( 
-		"POST /oauth2/invalidate_token HTTP/1.1", 
-		"Host: api.twitter.com", 
-		"User-Agent: jonhurlock Twitter Application-only OAuth App v.1",
-		"Authorization: Basic ".$base64_encoded_consumer_token."",
-		"Accept: */*", 
-		"Content-Type: application/x-www-form-urlencoded", 
-		"Content-Length: ".(strlen($bearer_token)+13).""
-	); 
-    
-	$ch = curl_init();  // setup a curl
-	curl_setopt($ch, CURLOPT_URL,$url);  // set url to send to
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); // set custom headers
-	curl_setopt($ch, CURLOPT_POST, 1); // send as post
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // return output
-	curl_setopt($ch, CURLOPT_POSTFIELDS, "access_token=".$bearer_token.""); // post body/fields to be sent
-	$header = curl_setopt($ch, CURLOPT_HEADER, 1); // send custom headers
-	$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-	$retrievedhtml = curl_exec ($ch); // execute the curl
-	curl_close($ch); // close the curl
-	return $retrievedhtml;
-}
-
-/**
-* Search
-* Basic Search of the Search API
-* Based on https://dev.twitter.com/docs/api/1/get/search
-*/
-function search_for_a_term($bearer_token, $query, $result_type='mixed', $rpp='15'){
-	$url = "https://api.twitter.com/1.1/search/tweets.json"; // base url
-	$q = $query; // query term
-
-	$formed_url ='?q='.$q; // fully formed url
-	if($result_type!='mixed'){$formed_url = $formed_url.'&result_type='.$result_type;} // result type - mixed(default), recent, popular
-	if($rpp!='15'){$formed_url = $formed_url.'&rpp='.$rpp;} // results per page - defaulted to 15
-	$formed_url = $formed_url.'&include_entities=true'; // makes sure the entities are included, note @mentions are not included see documentation
-	$headers = array( 
-		"GET /1.1/search/tweets.json".$formed_url." HTTP/1.1", 
-		"Host: api.twitter.com", 
-		"User-Agent: jonhurlock Twitter Application-only OAuth App v.1",
-		"Authorization: Bearer ".$bearer_token."",
-	);
-	$ch = curl_init();  // setup a curl
-	curl_setopt($ch, CURLOPT_URL,$url.$formed_url);  // set url to send to
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); // set custom headers
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // return output
-	$retrievedhtml = curl_exec ($ch); // execute the curl
-	curl_close($ch); // close the curl
-	return $retrievedhtml;
-}
-
-// lets run a search.
-$bearer_token = get_bearer_token(); // get the bearer token
-print search_for_a_term($bearer_token, "test"); //  search for the work 'test'
-invalidate_bearer_token($bearer_token); // invalidate the token
-?>
